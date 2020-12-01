5b6007740b936021252e44fc846c72bae19a2cad
bitbucket-cores-vn <bitbucket.cores.vn@gmail.com>
2020-12-01 15:23:24 +0700

    udpate code

---
+++ src/includes/validation.js
@@ -0,0 +1,15 @@
+const Authorization = require('./authorization')
+
+class Validation {
+    constructor() {
+        this.userId = 0;
+    }
+    async canDoAction ( req, cap,  args, validatedParams = [] ) {
+        const auth = await Authorization.authorize(req, cap);
+        if ( auth == true ) {
+            return 200;
+        }
+        return 401;
+    };
+}
+module.exports = new Validation();
