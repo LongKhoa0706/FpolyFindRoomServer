8b9804118737ebf475c0b76a9cf910ce901cdb11
bitbucket-cores-vn <bitbucket.cores.vn@gmail.com>
2020-12-16 15:55:28 +0700

    udpate code

--- index.js
+++ index.js
@@ -14,6 +14,25 @@ app.use(bodyParser.json());
 const connectDB = require('./src/config/config')
 connectDB();
 
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  if (req.method === "OPTIONS") {
+      res.header(
+          "Access-Control-Allow-Headers",
+          "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+      );
+      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+      return res.status(200).json({});
+  }
+  next();
+});
+
 app.get('/', (req, res) => res.send('Hello World!'))
 app.use(generalRouter)
 app.listen(port, () => console.log(`Server listing port: ${port}`))
